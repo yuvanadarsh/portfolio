1e1584121ae5eda8dfb7167cd0a2d28d82ad3042
Yuvan Adarsh Jagannathan <yuvanadarshj@gmail.com>
2026-03-16 17:10:49 -0400

    Added additional animation for hover

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,7 @@
 const Skills = ({ name, logo }) => {
   return (
     <>
-      <div className="border-2 solid border-white/10 rounded-2xl p-[60px] py-[10px] w-[100px] flex flex-col items-center bg-white/5 hover:bg-white/10 transition ease-in-out duration-200 gap-[10px]">
+      <div className="border-2 solid border-white/10 rounded-2xl p-[60px] py-[10px] w-[100px] flex flex-col items-center bg-white/5 hover:bg-white/10 hover:translate-y-[-5px] transition ease-in-out duration-200 gap-[10px]">
         <div className={`w-[60px] ${name === "Flask" ? "invert" : ""}`}>{logo}</div>
         <p className=" text-white ">{name}</p>
       </div>
