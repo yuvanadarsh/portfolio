0f655bb48ea15e6a1a3638818c20d7fe7c54a2c6
Yuvan Adarsh Jagannathan <yuvanadarshj@gmail.com>
2026-03-25 23:28:23 -0400

    added Education card

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Projects from "./components/Projects";
 // import profile from "./assets/profile.png";
 import skills_data from "./data/skills";
 import projects_data from "./data/projects";
+import logo from "./assets/umd-logo.png";
 
 const App = () => {
   // let getTemporary = (x, y) => {
@@ -37,6 +38,20 @@ const App = () => {
   return (
     <>
       <Navbar></Navbar>
+      <div id="projects" className="flex flex-col items-center mx-10 md:mx-20 lg:mx-40 pt-20 ">
+        <h3 className="text-3xl font-bold my-6 text-white">Projects</h3>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 items-stretch">
+          {projects_data.map((project, index) => {
+            return <Projects key={index} name={project.name} stack={project.stack} description={project.description}></Projects>;
+          })}
+          <div className="card-base items-center justify-center sm:py-5 sm:px-10 py-2.5 px-5">
+            <svg width="100px" height="100px" fill="white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path d="M12 4C9.243 4 7 6.243 7 9h2c0-1.654 1.346-3 3-3s3 1.346 3 3c0 1.069-.454 1.465-1.481 2.255-.382.294-.813.626-1.226 1.038C10.981 13.604 10.995 14.897 11 15v2h2v-2.009c0-.024.023-.601.707-1.284.32-.32.682-.598 1.031-.867C15.798 12.024 17 11.1 17 9c0-2.757-2.243-5-5-5zm-1 14h2v2h-2z" />
+            </svg>
+            <p>Something's cooking...</p>
+          </div>
+        </div>
+      </div>
       <div id="skills" className="flex flex-col items-center mx-10 md:mx-20 lg:mx-40 pt-20">
         <h3 className="text-3xl font-bold my-6 text-white">Skills</h3>
         <div className="flex flex-wrap gap-3 items-center justify-center">
@@ -45,17 +60,22 @@ const App = () => {
           })}
         </div>
       </div>
-      <div id="projects" className="flex flex-col items-center mx-10 md:mx-20 lg:mx-40 pt-20 ">
-        <h3 className="text-3xl font-bold my-6 text-white">Projects</h3>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 items-stretch">
-          {projects_data.map((project, index) => {
-            return <Projects key={index} name={project.name} stack={project.stack} description={project.description}></Projects>;
-          })}
-          <div className="card-base items-center justify-center">
-            <svg width="100px" height="100px" fill="white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path d="M12 4C9.243 4 7 6.243 7 9h2c0-1.654 1.346-3 3-3s3 1.346 3 3c0 1.069-.454 1.465-1.481 2.255-.382.294-.813.626-1.226 1.038C10.981 13.604 10.995 14.897 11 15v2h2v-2.009c0-.024.023-.601.707-1.284.32-.32.682-.598 1.031-.867C15.798 12.024 17 11.1 17 9c0-2.757-2.243-5-5-5zm-1 14h2v2h-2z" />
-            </svg>
-            <p>Something's cooking...</p>
+
+      <div id="education" className="flex flex-col items-center mx-10 md:mx-20 lg:mx-40 pt-20">
+        <h3 className="text-3xl font-bold my-6 text-white">Education</h3>
+        <div className="card-base flex items-center sm:py-5 sm:px-10 py-2.5 px-5 w-auto gap-2.5 text-white">
+          <h2 className="text-xl font-bold text-white">University of Maryland, College Park</h2>
+          <img src={logo} className="w-20 " alt="" />
+          <p>Bachelor of Science, Computer Science</p>
+          <div className="flex flex-col items-center text-neutral-300 text-[0.9rem]">
+            <p>GPA: 3.343</p>
+            <p>Expected Graduation: May 2028</p>
+          </div>
+          <h2 className="text-[1rem] font-bold text-white">Relavent Coursework</h2>
+          <div className="flex flex-col items-center text-neutral-300 text-[0.9rem]">
+            <p>CMSC216: Computer Systems</p>
+            <p>CMSC250: Discrete Structures</p>
+            <p>CMSC132: Object Oriented Progreamming II</p>
           </div>
         </div>
       </div>
